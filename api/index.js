229c87be3686265864d0718ac0029f8e2e36835d
emerrox <maurocordal@gmail.com>
2024-12-05 13:33:13 +0100

    security

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const usersRoutes = require('./routes/users');
 const groupsRoutes = require('./routes/groups');
 const groupsUsersRoutes = require('./routes/groups_users');
 
 const app = express();
 app.use(express.json());
+
+app.use(helmet());
+app.use(cors({ origin: 'https://tudominio.com' }));
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
 // Montar las rutas
 app.use('/api/users', usersRoutes);
