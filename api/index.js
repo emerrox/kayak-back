8fdf05264ff2470062b01e214678dd39031d66ae
emerrox <maurocordal@gmail.com>
2024-12-10 14:43:07 +0100

    cors ok

--- api/index.js
+++ api/index.js
@@ -1,23 +1,38 @@
 const express = require('express');
-const helmet = require('helmet');
 const cors = require('cors');
-// const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const usersRoutes = require('./routes/users');
 const groupsRoutes = require('./routes/groups');
 const groupsUsersRoutes = require('./routes/groups_users');
 const loginRoutes = require('./routes/login')
 const logoutRoutes = require('./routes/logout')
-// require('dotenv').config();
+const helmet = require('helmet');
+
 const app = express();
+
 app.use(express.json());
-
-// app.use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' }));
+app.use(helmet.referrerPolicy({ policy: 'unsafe-url' }))
 app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://kayak-plus.vercel.app'
+    ]
 
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  },
+  credentials: true,
+}))
+app.disable('x-powered-by')
 app.use(cookieParser())
 
 app.use('/api/users', usersRoutes);
