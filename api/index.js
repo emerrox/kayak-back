6b3289c588d01a8f1156ec4294ab39cb3fb338da
emerrox <maurocordal@gmail.com>
2024-12-10 16:09:43 +0100

    fix utils

--- api/index.js
+++ api/index.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const usersRoutes = require('./routes/users');
-const groupsRoutes = require('./routes/groups');
-const groupsUsersRoutes = require('./routes/groups_users');
-const loginRoutes = require('./routes/login')
-const logoutRoutes = require('./routes/logout')
-const helmet = require('helmet');
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import usersRoutes from './routes/users.js';
+import groupsRoutes from './routes/groups.js';
+import groupsUsersRoutes from './routes/groups_users.js';
+import loginRoutes from './routes/login.js';
+import logoutRoutes from './routes/logout.js';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.json());
-app.use(helmet.referrerPolicy({ policy: 'unsafe-url' }))
+app.use(helmet.referrerPolicy({ policy: 'unsafe-url' }));
 app.use(cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
       'http://localhost:3000',
       'http://localhost:5173',
-      'https://kayak-plus.vercel.app'
-    ]
+      'https://kayak-plus.vercel.app',
+      'https://kayak-back.vercel.app',
+      'https://kayak-*'
+    ];
 
     if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
+      return callback(null, true);
     }
 
     if (!origin) {
-      return callback(null, true)
+      return callback(null, true);
     }
 
-    return callback(new Error('Not allowed by CORS'))
+    return callback(new Error('Not allowed by CORS'));
   },
   credentials: true,
-}))
-app.disable('x-powered-by')
-app.use(cookieParser())
+}));
+
+app.disable('x-powered-by');
+app.use(cookieParser());
 
 app.use('/api/users', usersRoutes);
 app.use('/api/groups', groupsRoutes);
@@ -41,8 +44,8 @@ app.use('/api/groups_users', groupsUsersRoutes);
 app.use('/api/login', loginRoutes);
 app.use('/api/logout', logoutRoutes);
 
-  app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-  });
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
 
-module.exports = app;
+export default app;
--- api/routes/groups.js
+++ api/routes/groups.js
@@ -1,6 +1,7 @@
-const { Router } = require('express');
-const { readJSON, writeJSON } = require('../utils');
-const authenticate = require('../authenticate'); 
+import { Router } from 'express';
+import { readJSON, writeJSON } from '../utils.js';
+import authenticate from '../authenticate.js';
+
 const router = Router();
 
 router.get('/', authenticate, (req, res) => {
@@ -9,46 +10,42 @@ router.get('/', authenticate, (req, res) => {
 });
 
 router.get('/:id', authenticate, (req, res) => {
-  const groups = readJSON('groups.json')
+  const groups = readJSON('groups.json');
   const group = groups.find(g => g.id === req.params.id);
   if (group) {
-    const users = readJSON('groups_users.json')
-    let user = []
-    users.map((u)=>{
-      if (u.userId==req.params.id) {
-        user.push(u.userId)
+    const users = readJSON('groups_users.json');
+    let user = [];
+    users.map((u) => {
+      if (u.userId == req.params.id) {
+        user.push(u.userId);
       }
-    })
-    group.users = user
+    });
+    group.users = user;
     res.json(group);
   } else {
     res.status(404).json({ message: 'Grupo no encontrado' });
   }
 });
 
-/***** TO DO ******/
-// falta que al crear el grupo se añada el usuario que lo crea
 // Ruta para crear un nuevo grupo
 router.post('/', authenticate, (req, res) => {
   const { name } = req.body;
 
-  
-  const groups = readJSON('groups.json')
+  const groups = readJSON('groups.json');
   const newGroup = {
     id: (groups.length + 1).toString(),
     name: name
   };
   groups.push(newGroup);
-  writeJSON('groups.json',groups)
-
+  writeJSON('groups.json', groups);
 
   res.status(201).json(newGroup);
 });
 
 router.delete('/:id', authenticate, (req, res) => {
-  let groups = readJSON('groups.json')
+  let groups = readJSON('groups.json');
   groups = groups.filter(g => g.id !== req.params.id);
-  writeJSON('groups.json', groups)
+  writeJSON('groups.json', groups);
 
   let users_g = readJSON('groups_users.json');
   users_g = users_g.filter(u => u.groupId !== req.params.id);
@@ -57,4 +54,4 @@ router.delete('/:id', authenticate, (req, res) => {
   res.json({ message: 'Grupo eliminado' });
 });
 
-module.exports = router;
+export default router;
